50fcb38c5f2c385f5c46cb65e176e6bd0dd91696
Shahrukh <shahrukh1494@gmail.com>
2024-06-21 10:31:17 +0530

    install and configure helmet, and add meta tags with helmet in shop route

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,17 @@
+import { Helmet } from "react-helmet-async";
+
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Shop</title>
+        <meta name="description" content="shop our latest products now." />
+        <link rel="canonical" href="http://localhost:3000/shop" />
+      </Helmet>
+
+      <div>Shop</div>
+    </>
+  );
 };
 
 export default Shop;
