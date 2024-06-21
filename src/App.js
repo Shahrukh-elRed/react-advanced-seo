d94b7187ed5675b6799d489314ff818f5c537c3b
Shahrukh <shahrukh1494@gmail.com>
2024-06-21 10:06:18 +0530

    added routing and login and shop routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./Login";
+import Shop from "./Shop";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
